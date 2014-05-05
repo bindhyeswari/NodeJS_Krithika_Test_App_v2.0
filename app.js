8e3a4f2dfea96b90e60dfde870aaa6a1a387c1f9
Krithika Swaminathan <krithika1969@yahoo.com>
2014-05-05 12:42:51 -0700

    data files

---
+++ app.js
@@ -0,0 +1,76 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser({uploadDir: './uploadedData'}));
+app.use(express.multipart());
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+app.get('/uitest', function(req, res){
+    res.render('uitest');
+});
+
+app.get('/tests/lists', function(req, res){
+    console.log(req.body);
+    res.render('lists', { message: req.body});
+});
+
+app.get('/tests/:num', function(req, res){
+    console.log(req.params);
+    res.render('tests', { id: req.params.num, description: 'Dummy'
+    });
+});
+
+app.get('/test.json/:id', function(req, res){
+    res.json(200, {data: [{
+        question: 'What is the most popular MVC front end framework?',
+        options: ["Backbone JS" , "Angular JS" , "MongoDB" , "Symphony PHP"]}
+        ]});
+
+});
+
+app.post('/tests/test_submitted/:num', function(req, res){
+    console.log(req.body);
+    res.render('test_submitted', { id: req.body.test_id , description: req.body.test_desc});
+});
+
+app.post('/signup', function(req, res){
+    console.log(req.body);
+    res.render('signup', { message: req.body});
+});
+
+app.post('/tests/upload', function(req, res){
+    console.log(req.body);
+    res.render('upload', { message: req.body});
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
